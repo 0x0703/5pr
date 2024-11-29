3f108e6f22002446679fb025eddc39bb2e10f63e
0x0703 <gordonmccannm4321@gmail.com>
2024-11-29 09:15:46 +0300

    Update list_users.c

--- list_users.c
+++ list_users.c
@@ -1,5 +1,22 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <pwd.h>
+#include <string.h>
 
 void list_users() {
-    system("awk -F: '{ print $1, $6 }' /etc/passwd | sort -k1");
+    struct passwd *pw;
+    FILE *fp = popen("cut -d: -f1,6 /etc/passwd | sort", "r");
+    
+    if (fp == NULL) {
+        perror("Ошибка при выполнении команды");
+        return;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        printf("%s", line);
+    }
+    
+    pclose(fp);
 }
+
